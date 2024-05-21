20761648c9ad3fd9185e91a5ae15045e1acdf782
Caio Monte <168784455+osmozeInc@users.noreply.github.com>
2024-05-21 17:35:56 -0300

    Add files via upload

---
+++ Desafios/desafio_sistema_bancario.py
@@ -0,0 +1,49 @@
+import os
+os.system('cls')
+saldo = 1500
+extrato = []
+
+input("Sistema Bancário \npressione enter para prosseguir ou sair\n")
+escolha = input("o que deseja fazer: \n1 - Sacar \n2 - Depositar \n3 - Visualizar Extrato \n--> ")
+
+while True:
+
+    if str(escolha) == "": break
+
+
+    elif int(escolha) == 1:
+        os.system('cls')
+        saque = float(input(f"Extrato: R$ {saldo:.2f} \nqual o valor do saque?\n --> "))
+        os.system('cls')
+        if saldo >= saque and saque > 0: 
+            print(f"sacando: R$ {saque:.2f} \nExtrato atual: R$ {(saldo - saque):.2f}")
+            extrato.append(f"sacado o valor de R$ {saque}")
+        else: print(f"Saldo Insuficiente \nSaldo: R$ {saldo:.2f}")
+        saldo = saldo - saque
+        escolha = input("\n\no que deseja fazer: \n1 - Sacar \n2 - Depositar \n3 - Visualizar Extrato \n--> ")
+
+
+    elif int(escolha) == 2:
+        os.system('cls')
+        deposito = float(input(f"Extrato: R$ {saldo:.2f} \nqual o valor do deposito?\n --> "))
+        os.system('cls')
+        print(f"Depositando R$ {deposito} \nExtrato atual: R$ {deposito + saldo}")
+        extrato.append(f"depositado o valor de R$ {saque}\n")
+        saldo = deposito + saldo
+        escolha = input("\n\no que deseja fazer: \n1 - Sacar \n2 - Depositar \n3 - Visualizar Extrato \n--> ")
+
+
+    elif int(escolha) == 3:
+        os.system('cls')
+        print(f"Saldo atual: R$ {saldo}")
+        print(extrato)
+        escolha = input("\n\no que deseja fazer: \n1 - Sacar \n2 - Depositar \n3 - Visualizar Extrato \n--> ")
+
+        
+
+    else:
+        os.system('cls')
+        print("Valor inválido")
+        escolha = input("\no que deseja fazer: \n1 - Sacar \n2 - Depositar \n3 - Visualizar Extrato \n--> ")
+os.system('cls')
+print("\nFechando Sistema...\n\n")
