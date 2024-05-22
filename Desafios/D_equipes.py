2ab2b2c8181748f4039a29377349040277146725
Caio Monte <168784455+osmozeInc@users.noreply.github.com>
2024-05-22 19:59:43 +0000

    d

--- Desafios/D_equipes.py
+++ Desafios/D_equipes.py
@@ -1,14 +1,8 @@
 lista_completa = set(["Caio", "Davi", "Edmilson", "Erick", "Esmael", "Alison", 'Expedito', 'Armando', 'Kauã', 'Marcos', 'Graziel', "Raiane", "Natanael", "Gabriel", "Pedro", "Silas"])
-equipe_1 = []
-equipe_2 = []
-equipe_3 = []
-equipe_4 = []
+equipes = [[], [], [], []]
 
 for i in range(4):
-    equipe_1.append(lista_completa.pop())
-    equipe_2.append(lista_completa.pop())
-    equipe_3.append(lista_completa.pop())
-    equipe_4.append(lista_completa.pop())
+        for j in range(4):
+            equipes[i].append(lista_completa.pop()) 
 
-print(f"equipe 1: \n {equipe_1} \nGerente: {equipe_1.pop()}\n\n equipe 2: \n {equipe_2} \nGerente: {equipe_2.pop()}\n")
-print(f"equipe 3: \n {equipe_3} \nGerente: {equipe_3.pop()}\n\n equipe 4: \n {equipe_4} \nGerente: {equipe_4.pop()}\n\n")
+for i in range(4): print(f"equipe {i+1}: \n {equipes[i]} \nGerente: {equipes[i].pop()}\n")
