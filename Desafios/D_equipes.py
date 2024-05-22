310ed247687c3183c7cffb33970d1fa64f809399
Caio Monte <168784455+osmozeInc@users.noreply.github.com>
2024-05-22 20:17:24 +0000

    a

--- Desafios/D_equipes.py
+++ Desafios/D_equipes.py
@@ -1,8 +1,9 @@
-lista_completa = set(["Caio", "Davi", "Edmilson", "Erick", "Esmael", "Alison", 'Expedito', 'Armando', 'Kauã', 'Marcos', 'Graziel', "Raiane", "Natanael", "Gabriel", "Pedro", "Silas"])
-equipes = [[], [], [], []]
+import random
+equipes = [['Raiane', 'Felipe', 'Caio', 'Kauã'],
+            ['Pedro', 'Expedito', 'Alison', 'Silas'],
+              ['Esmael', 'Armando', 'Natanael', 'Erick'],
+                ['Davi', 'Marcos', 'Gabriel', 'Graziel']]
+ 
 
-for i in range(4):
-        for j in range(4):
-            equipes[i].append(lista_completa.pop()) 
-
-for i in range(4): print(f"equipe {i+1}: \n {equipes[i]} \nGerente: {equipes[i].pop()}\n")
+for i in range(4): print(f"equipe {i+1}: \n {equipes[i]} \nGerente: {random.choice(equipes[i])
+}\n")
