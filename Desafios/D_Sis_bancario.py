c52bd843cb81fa9d6529eb93cd55ca784071edf3
Caio Monte <168784455+osmozeInc@users.noreply.github.com>
2024-05-28 02:33:06 -0300

    desafio feito

--- Desafios/D_Sis_bancario.py
+++ Desafios/D_Sis_bancario.py
@@ -1,50 +1,84 @@
 import os
 os.system('cls')
 saldo = 1500
+saldo = float(saldo)
 extrato = []
 
-input("Sistema Bancário \npressione enter para prosseguir ou sair\n")
-escolha = input("o que deseja fazer: \n1 - Sacar \n2 - Depositar \n3 - Visualizar Extrato \n--> ")
+def Menu():
+    os.system('cls')
+    print(texto)
+    opcao = input("o que deseja fazer: \n1 - Sacar \n2 - Depositar \n3 - Visualizar Extrato \n--> ")
+    return opcao
+
+
+def Saque(saldo):
+    os.system('cls')
+    print(texto)
+    valor = input("Qual o valor do saque?\n--> ")
+    try:
+        valor = float(valor)
+        if valor <= saldo and valor > 0:
+            saldo -= valor
+            extrato.append(f"Sacado: R$ {valor:.2f}")
+            print(f"\nSaque de R$ {valor:.2f} realizado com sucesso.\nSaldo atual: R$ {saldo:.2f}")
+        else:
+            print("\nSaldo insuficiente!")
+    except ValueError:
+        print("\nValor inválido!")
+    input("\nPressione Enter para continuar...")
+    return saldo
+
+
+def Deposito(saldo):
+    os.system('cls')
+    print(texto)
+    valor = input("Qual o valor do deposito?\n--> ")
+    try:
+        valor = float(valor)
+        if valor > 0:
+            saldo += valor
+            extrato.append(f"Depositado: R$ {valor:.2f}")
+            print(f"\nDeposito de R$ {valor:.2f} realizado com sucesso.\nSaldo atual: R$ {saldo:.2f}")
+        else: 
+            print("\nValor inválido!")
+    except:
+        print("\nValor inválidoo!")
+    input("\nPressione Enter para continuar...")
+    return saldo
+
+
+def Extrato():
+    os.system('cls')
+    print(texto, "\n")
+    for operacao in extrato:
+        print(operacao)
+    input("\nPressione Enter para continuar...")
+
+
+
+texto = "        === Sistema Bancário === \n"
+print(texto)
+input("\nPressione Enter para continuar ou sair...")
 
 while True:
+    texto = f"Saldo: R$ {saldo:.2f}"
+    escolha = Menu()
 
-    if str(escolha) == "": break
+    if escolha == "": break
 
-
-    elif int(escolha) == 1:
-        os.system('cls')
-        saque = float(input(f"Extrato: R$ {saldo:.2f} \nqual o valor do saque?\n --> "))
-        os.system('cls')
-        if saldo >= saque and saque > 0: 
-            print(f"sacando: R$ {saque:.2f} \nExtrato atual: R$ {(saldo - saque):.2f}")
-            extrato.append(f"sacado o valor de R$ {saque}")
-            saldo = saldo - saque
-        else: print(f"Saldo Insuficiente \nSaldo: R$ {saldo:.2f}")
-        escolha = input("\n\no que deseja fazer: \n1 - Sacar \n2 - Depositar \n3 - Visualizar Extrato \n--> ")
-
-
-    elif int(escolha) == 2:
-        os.system('cls')
-        deposito = float(input(f"Extrato: R$ {saldo:.2f} \nqual o valor do deposito?\n --> "))
-        os.system('cls')
-        print(f"Depositando R$ {deposito:.2f} \nExtrato atual: R$ {(deposito + saldo):.2f}")
-        extrato.append(f"depositado o valor de R$ {deposito:.2f}\n")
-        saldo = deposito + saldo
-        escolha = input("\n\no que deseja fazer: \n1 - Sacar \n2 - Depositar \n3 - Visualizar Extrato \n--> ")
-
-
-    elif int(escolha) == 3:
-        os.system('cls')
-        print(f"Saldo atual: R$ {saldo:.2f}\n")
-        for operacao in extrato:
-            print(operacao)
-        escolha = input("\n\no que deseja fazer: \n1 - Sacar \n2 - Depositar \n3 - Visualizar Extrato \n--> ")
-
+    elif escolha == '1':
+        saldo = Saque(saldo)
         
+    elif escolha == '2':
+        saldo = Deposito(saldo)
 
-    else:
-        os.system('cls')
-        print("Valor inválido")
-        escolha = input("\no que deseja fazer: \n1 - Sacar \n2 - Depositar \n3 - Visualizar Extrato \n--> ")
-os.system('cls')
-print("\nFechando Sistema...\n\n")
+    elif escolha == '3':
+        Extrato()
+    
+    else: 
+        print("Valor inválido!")
+        input("\nPressione Enter para continuar...")
+
+    texto = f"Saldo: R$ {saldo:.2f}"
+
+print("\nFechando Programa...\n\n")
