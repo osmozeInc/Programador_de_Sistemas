4f6e3e1786191f436d4f05d217f47ab2d9d0c173
Caio Monte <168784455+osmozeInc@users.noreply.github.com>
2024-06-06 16:52:41 -0300

    conta bancaria

--- Desafios/D_Conta_Bancaria.py
+++ Desafios/D_Conta_Bancaria.py
@@ -13,11 +13,15 @@ class Banco:
     def Deposito(self):
         pass
 
+
+
 class Cliente(Banco):
-    def __init__(self, name = "", CPF = "", nascimento = ""):
+    def __init__(self, name = "", CPF = "", nascimento = "", email = "", celular = ""):
         self.usuario = name
         self.CPF = CPF
         self.nascimento = nascimento
+        self.email = email
+        self.celular = celular
         self.saldo = 0.00
 
     def Deposito(self):
@@ -88,7 +92,11 @@ class Cliente(Banco):
 
 
 
-class Interface():
+class Interface:
+    def __init__(self, cliente):
+        self.cliente = cliente
+        self.setings = Setings(cliente)
+
     def Menu(self):
         while True:
             sys.stdout.flush()
@@ -109,21 +117,27 @@ class Interface():
             else:
                 print("Opção invalida")
                 time.sleep(4)
-
-    def Criar_usuario(self):
+    
+    def Login_usuario(self):
         os.system('cls')
-        print("Deseja criar um novo usuario ou prosseguir anônimo? \nenter - Sim \nEsc - Não")
+        print("Deseja criar um novo usuario ou prosseguir anônimo? \nenter - Criar Usuário \nEsc - Proseguir Anônimo")
         while True:
             key = keyboard.read_key()
             if key == "enter":
-                Interface.Informações_do_usuario()
+                interface.Criar_usuario()
                 break
             elif key == "esc":
                 print("\nIndo para o menu anonimamente \nSeus dados estarão em branco")
-                time.sleep(5)
+                time.sleep(4.5)
                 break
 
-    def Informações_do_usuario():
+    def Criar_usuario(self):
+        os.system('cls')
+        print("Informações Pessoais")
+        Setings.Alterar_nome()
+        Setings.Alterar_CPF()
+        
+    def Modificar_usuario(self):
         os.system('cls')
         print("Informe seus dados ou pressione enter para prosseguir")
         
@@ -131,21 +145,58 @@ class Interface():
         cpf = input("CPF: ")
         nascimento = input("Nascimento: ")
 
+        if name != "":
+            self.cliente.usuario = name
+        if cpf != "":
+            self.cliente.CPF = cpf
+        if nascimento != "":
+            self.cliente.nascimento = nascimento
+        
 
-           
+
+class Setings:
+    def __init__(self, cliente):
+        self.cliente = cliente
+        self.caractere_especial = "!@#$%&*()_-+={[}]|\:;'<>?,./"
+        self.caractere_numero = "0123456789"
+        self.caractere_letra = "abcdefghijklmnopqrstuvwxyz"
+
+    def Alterar_nome(self):
+        while True:
+            os.system('cls')
+            name = input("Nome: ")
+            if self.caractere_especial in name or self.caractere_numero in name:
+                print("Nome não pode conter caracteres especiais ou numeros")
+                time.sleep(3)
+            else:
+                self.cliente.usuario = name
+            name = name.lower().title().strip()
+            break
+    
+    def Alterar_CPF(self):
+        while True:
+            os.system('cls')
+            cpf = input("CPF: ")
+            if self.caractere_especial in cpf or self.caractere_letra in cpf or len(cpf)!= 11:
+                print("Seu CPF deve ter 11 numeros e nenhum caractere especial ou letra")
+                time.sleep(3)
+            else:
+                cpf = cpf[:3] + "." + cpf[3:6] + "." + cpf[6:9] + "-" + cpf[9:]
+                self.cliente.usuario = cpf
+            break
 
 
 
 
 os.system('cls')
 cliente = Cliente()
-interface = Interface()
+interface = Interface(cliente)
 
 print("Iniciando Sistema Bancario")
 time.sleep(2)
 
 
-interface.Criar_usuario()
+interface.Login_usuario()
 interface.Menu()
 
 print("\nSaindo do sistema")
