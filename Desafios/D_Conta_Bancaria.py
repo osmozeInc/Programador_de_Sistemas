9f521c71b0be4bc5594e8cf11571a15babe15158
Caio Monte <168784455+osmozeInc@users.noreply.github.com>
2024-06-08 03:23:09 -0300

    conta bancaria

--- Desafios/D_Conta_Bancaria.py
+++ Desafios/D_Conta_Bancaria.py
@@ -1,7 +1,8 @@
 import os
 import time
-import keyboard  # "pip install keyboard" no prompt para instalar a biblioteca
+import keyboard  # type: ignore # "pip install keyboard" no prompt para instalar a biblioteca
 import sys
+import re
 
 class Banco:
     def __init__(self) -> None:
@@ -16,12 +17,13 @@ class Banco:
 
 
 class Cliente(Banco):
-    def __init__(self, name="", CPF="", nascimento="", email="", celular=""):
+    def __init__(self, name="", CPF="", nascimento="", email="", celular="", senha=""):
         self.usuario = name
         self.CPF = CPF
         self.nascimento = nascimento
         self.email = email
         self.celular = celular
+        self.senha = senha
         self.saldo = 0.00
 
     def Deposito(self):
@@ -49,7 +51,6 @@ class Cliente(Banco):
                 print("\nValor invalido! \nretomando operação")
                 time.sleep(3)
 
-
     def Saque(self):
         print("\nComeçando operação de Saque")
         time.sleep(3)
@@ -74,7 +75,6 @@ class Cliente(Banco):
             except:
                 print("\nValor invalido! \nretomando operação")
                 time.sleep(3)
-
 
     def Perfil(self):
         os.system('cls')
@@ -122,27 +122,28 @@ class Interface:
         os.system('cls')
         print("Deseja criar um novo usuario ou prosseguir anônimo? \nenter - Criar Usuário \nEsc - Proseguir Anônimo")
         while True:
-            key = keyboard.read_key()
-            if key == "enter":
+            key = keyboard.read_event()
+            if key.name == "enter":
                 self.Criar_usuario()
                 break
-            elif key == "esc":
+            elif key.name == "esc":
                 print("\nIndo para o menu anonimamente \nSeus dados estarão em branco")
-                time.sleep(4.5)
+                time.sleep(5)
                 break
 
     def Criar_usuario(self):
-        os.system('cls')
-        print("Informações Pessoais")
+        input()
         self.setings.Alterar_nome()
         self.setings.Alterar_CPF()
+        self.setings.Alterar_nascimento()
+        self.setings.Alterar_email()
+        self.setings.Alterar_celular()
+        self.setings.Alterar_senha()
         
     def Modificar_usuario(self):
         os.system('cls')
         print("Informe seus dados ou pressione enter para prosseguir")
         input("DEV: 'trabalando nessa parte ainda'")
-
-
 
 
 
@@ -152,10 +153,12 @@ class Setings:
         self.caractere_especial = "!@#$%&*()_-+={[}]|\:;'<>?,./"
         self.caractere_numero = "0123456789"
         self.caractere_letra = "abcdefghijklmnopqrstuvwxyz"
+        self.email_padrao = r'^[\w\.-]+@[\w\.-]+\.\w+$'
 
     def Alterar_nome(self):
         while True:
             os.system('cls')
+            sys.stdout.flush()
             name = input("Nome: ")
             if any(char in self.caractere_especial for char in name) or any(char in self.caractere_numero for char in name) or name == "":
                 print("Nome não pode conter caracteres especiais ou numeros")
@@ -163,19 +166,76 @@ class Setings:
             else:
                 name = name.lower().title().strip()
                 self.cliente.usuario = name
-            break
+                break
 
     def Alterar_CPF(self):
         while True:
             os.system('cls')
             cpf = input("CPF: ")
-            if any(char in self.caractere_especial for char in cpf) in any(char in self.caractere_numero for char in cpf) or len(cpf)!= 11 or cpf == "":
+            if any(char in self.caractere_especial for char in cpf) or any(char in self.caractere_letra for char in cpf) or len(cpf)!= 11:
                 print("Seu CPF deve ter 11 numeros e nenhum caractere especial ou letra")
                 time.sleep(3)
             else:
-                cpf = cpf[:3] + "." + cpf[3:6] + "." + cpf[6:9] + "-" + cpf[9:]
-                self.cliente.CPF = cpf
-            break
+                self.cliente.CPF = cpf[:3] + "." + cpf[3:6] + "." + cpf[6:9] + "-" + cpf[9:]
+                break
+
+    def Alterar_nascimento(self):
+        while True:
+            os.system('cls')
+            nascimento = input("Data de nascimento \nDia: ")
+            nascimento += input("Mês: ")
+            nascimento += input("Ano: ")
+            if any(char in self.caractere_especial for char in nascimento) or any(char in self.caractere_letra for char in nascimento) or len(nascimento)!= 8:
+                print("Seu nascimento deve ter 8 numeros e nenhum caractere especial")
+                time.sleep(3)
+            else:
+                self.cliente.nascimento = nascimento[:2] + "/" + nascimento[2:4] + "/" + nascimento[4:]
+                break
+
+    def Alterar_email(self):
+        while True:
+            os.system('cls')
+            email = input("Email: ")
+            if not re.match(self.email_padrao, email):
+                print("Email inválido!")
+                time.sleep(3)
+            else:
+                self.cliente.email = email
+                break
+
+    def Alterar_celular(self):
+                while True:
+                    os.system('cls')
+                    celular = input("Numero do celular \n+55 DDD: ")
+                    if any(char in self.caractere_especial for char in celular) or any(char in self.caractere_letra for char in celular) or len(celular)!= 2:
+                        print("DDD invalido!")
+                        time.sleep(3)
+                    else:
+                        while True:
+                            os.system('cls')
+                            celular += input(f"Numero do celular \n+55 ({celular}) 9")
+                            if any(char in self.caractere_especial for char in celular) or any(char in self.caractere_letra for char in celular) or len(celular)!= 10:
+                                print("Seu celular deve ter 8 numeros e nenhum caractere especial")
+                                time.sleep(3)
+                            else:
+                                self.cliente.celular = celular
+                                break
+                        break
+
+    def Alterar_senha(self):
+                while True:
+                    os.system('cls')
+                    
+                    print("Senha: ", end="")
+                    senha_1 = 
+                    print("Confirme sua senha: ", end="")
+                    senha_2 = 
+                    if senha_1 != senha_2:
+                        print("As senhas não coincidem!")
+                        time.sleep(3)
+                    else:
+                        self.cliente.senha = senha_1
+                        break
 
 
 
@@ -183,10 +243,11 @@ class Setings:
 os.system('cls')
 cliente = Cliente()
 interface = Interface(cliente)
+settings = Setings(cliente)
 
 print("Iniciando Sistema Bancário")
 time.sleep(2)
-
+settings.Alterar_senha()
 
 interface.Login_usuario()
 interface.Menu()
@@ -194,3 +255,8 @@ interface.Menu()
 print("\nSaindo do sistema")
 time.sleep(2)
 os.system('cls')
+
+
+
+# procurar biblioteca para manipular calendário e ajeitar a parte de alterar_nascimento
+# procurar biblioteca para cmuflar a senha quando o usuario digitar
