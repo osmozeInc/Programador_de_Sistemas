a9d2c74a76597d3bef3f5e20bf146ea454de0251
Caio Monte <168784455+osmozeInc@users.noreply.github.com>
2024-06-12 23:36:37 -0300

    correção de erros

--- Projeto_Bancario/D_Conta_Bancaria.py
+++ Projeto_Bancario/D_Conta_Bancaria.py
@@ -208,7 +208,6 @@ class Setings:
             if any(char in self.caractere_especial for char in cpf) or any(char.isalpha() for char in cpf) or len(cpf)!= 11:
                 print("Seu CPF deve ter 11 numeros e nenhum caractere especial ou letra")
                 time.sleep(3)
-                self.Alterar_CPF()
             else:
                 self.cliente.CPF = cpf[:3] + "." + cpf[3:6] + "." + cpf[6:9] + "-" + cpf[9:]
                 break
@@ -254,7 +253,7 @@ class Setings:
                 while True:
                     os.system('cls')
                     celular += input(f"Numero do celular \n+55 ({celular}) 9")
-                    if any(char in self.caractere_especial for char in celular) or any(char in self.caractere_letra for char in celular) or len(celular)!= 10:
+                    if any(char in self.caractere_especial for char in celular) or any(char.isalpha() for char in celular) or len(celular)!= 10:
                         print("Seu celular deve ter 8 numeros e nenhum caractere especial")
                         time.sleep(3)
                     else:
