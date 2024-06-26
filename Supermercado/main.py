61c9371669f5d843b39dfe1f11877a695cd190a8
Caio Monte <168784455+osmozeInc@users.noreply.github.com>
2024-06-26 16:27:31 -0300

    projeto de supermercado atualizado

--- Supermercado/main.py
+++ Supermercado/main.py
@@ -7,7 +7,7 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.spinner import Spinner, SpinnerOption
 from kivy.core.window import Window
-import Dados
+import dados
 
 
 class Banco_de_Dados:
