96b1960fefa0188e7f8efba1dd6c332366f86ce9
Caio Monte <168784455+osmozeInc@users.noreply.github.com>
2024-06-26 16:29:41 -0300

    Create dados.py

---
+++ Supermercado/dados.py
@@ -0,0 +1,28 @@
+class Dados_de_cadastro:
+    def VerificNome(nome):
+        if nome == '':
+            return 'Erro', nome
+        
+    def VerificCPF(cpf):
+        pass
+
+    def VerificDDD(ddd):
+        pass
+
+    def VerificNumero(numero):
+        pass
+
+    def VerificEmail(email):
+        pass
+
+    def VerificDepartamento(departamento):
+        pass
+
+    def VerificCargo(cargo):
+        pass
+
+    def VerificSenha(senha):
+        pass
+
+    def VerificConfirmSenha(ConfirmSenha):
+        pass
