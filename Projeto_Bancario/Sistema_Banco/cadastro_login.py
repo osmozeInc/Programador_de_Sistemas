e3190075e1bbd414697da64ea958efb4ee6ca763
Caio Monte <168784455+osmozeInc@users.noreply.github.com>
2024-07-09 00:46:56 -0300

    Update cadastro_login.py

--- Projeto_Bancario/Sistema_Banco/cadastro_login.py
+++ Projeto_Bancario/Sistema_Banco/cadastro_login.py
@@ -92,4 +92,8 @@ def VerificConfirmSenha(ConfirmSenha, senha):
     elif ConfirmSenha != senha:
         return 'As senhas não coincidem', ConfirmSenha
     else:
+        return ' ', ConfirmSenha        return 'Preencha esse campo', ConfirmSenha
+    elif ConfirmSenha != senha:
+        return 'As senhas não coincidem', ConfirmSenha
+    else:
         return ' ', ConfirmSenha
